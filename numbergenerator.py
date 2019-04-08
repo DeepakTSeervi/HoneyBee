55f60ab1f1969cfdc487811fe15233643c85bc45
RaptorDOS <43664460+RaptorDOS@users.noreply.github.com>
2019-04-08 13:02:19 +0530

    stuff u need to add

---
+++ numbergenerator.py
@@ -0,0 +1,3 @@
+import random
+for i in range(0,10001):
+        print(random.randint(0, 20000), end = ' ')
---
+++ shellscr.sh
@@ -0,0 +1,4 @@
+python3 numbergenerator.py > numbersforArray.txt
+sort -g numbersforArray.txt
+gcc honey.c
+./a.out
